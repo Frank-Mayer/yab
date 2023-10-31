280733e9156caf732c9004368db32ed4ec7d9fca
Frank Mayer <git@frank-mayer.io>
2023-10-31 17:47:06 +0100

    extract gopath

--- .selene/install.lua
+++ .selene/install.lua
@@ -1,3 +1,4 @@
 local bin_name = require("build")
+local gopath = os.getenv("GOPATH")
 
-os.rename(bin_name, os.getenv("GOPATH") .. "/bin/" .. bin_name)
+os.rename(bin_name, gopath .. "/bin/" .. bin_name)
