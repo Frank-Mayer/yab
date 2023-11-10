dea432a3170b4052a695f2c641427aac3eb6522b
Frank Mayer <git@frank-mayer.io>
2023-11-10 01:25:36 +0100

    added check for GOPATH variable

--- .selene/install.lua
+++ .selene/install.lua
@@ -1,4 +1,8 @@
 local bin_name = require("build")
 local gopath = os.getenv("GOPATH")
+if gopath == nil then
+    print("GOPATH is not set")
+    os.exit(1)
+end
 
 os.rename(bin_name, gopath .. "/bin/" .. bin_name)
